8198dd85d73128c10a5f2b1ced51eeb108dca96e
rquanx <runquant@gmail.com>
2025-03-12 22:46:33 +0800

    feat: remove pino logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,13 @@
-import pino from 'pino'
-import path from 'pathe'
+// import pino from 'pino'
+// import path from 'pathe'
 
+// export const logger = pino({
+//   transport: {
+//     target: roll,
+//     options: { file: path.join(import.meta.dirname, 'logs', 'app'), frequency: 'daily', mkdir: true, extension: '.log', dateFormat: 'yyyy-MM-dd' }
+//   },
+// })
 
-const transport = pino.transport({
-  target: 'pino-roll',
-  options: { file: path.join('logs', 'app'), frequency: 'daily', mkdir: true, extension: '.log', dateFormat: 'yyyy-MM-dd' }
-})
-
-export const logger = pino(transport)
+export const logger = (...args: any[]) => {
+  // console.log(111, ...args)
+}
