437f87245b30d16c181c5263ec3f8e5a79216516
rquanx <runquant@gmail.com>
2025-03-12 22:16:13 +0800

    feat: logger rotation

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,29 +1,10 @@
 import pino from 'pino'
-import path from 'path'
-import fs from 'fs'
-import * as rfs from "rotating-file-stream";
-
-// 创建一个 rotating-file-stream 实例
-const stream = rfs.createStream("app.log", {
-  interval: "1d", // 每天轮转一次
-  compress: "gzip", // 压缩旧日志文件
-});
-
-// 确保 logs 目录存在
-const logsDir = path.join(process.cwd(), 'logs')
-if (!fs.existsSync(logsDir)) {
-  fs.mkdirSync(logsDir, { recursive: true })
-}
+import path from 'pathe'
 
 
-// 创建 logger 实例
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: false,
-      translateTime: 'SYS:standard',
-      destination: stream,
-    },
-  },
+const transport = pino.transport({
+  target: 'pino-roll',
+  options: { file: path.join('logs', 'app'), frequency: 'daily', mkdir: true, extension: '.log', dateFormat: 'yyyy-MM-dd' }
 })
+
+export const logger = pino(transport)
